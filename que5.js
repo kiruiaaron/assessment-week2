0ef60a814fd41376a9b48de2378946113812b3f8
kiruiaaron <kiruiaaron471@gmail.com>
2023-05-12 11:39:01 +0300

    a js file to find the sum between two numbers and a string

---
+++ que5.js
@@ -0,0 +1,5 @@
+let sum = 5 + 5 + "5";
+
+console.log(sum);
+
+//The output of the above code is 105 this is because the first two values are integer type and the third one is a string and therefore because of the + operator the addition of the first two integers is concatenated with the value of the string which is 5 and therefore sum of of the two integers gives 10 and when concatenated with the string value "5" makes it 105
